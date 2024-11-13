235130cf72cbae1b6952a9f64cbe2a9d90fb877f
tehw0lf <tehwolf@protonmail.com>
2024-11-13 20:34:46 +0100

    Add separate reset functions for session and user

--- apps/number-game/src/app/services/state.service.ts
+++ apps/number-game/src/app/services/state.service.ts
@@ -11,13 +11,11 @@ import { Player, SessionUser, UserInfo } from '@number-game/core';
   providedIn: 'root',
 })
 export class StateService {
-  userInfo: WritableSignal<UserInfo | null> = signal(null);
   sessionUser: WritableSignal<SessionUser | null> = signal(null);
-
   players: WritableSignal<Player[]> = signal([]);
-
+  userInfo: WritableSignal<UserInfo | null> = signal(null);
   userGuess: WritableSignal<number> = signal(-1);
-
+  session: WritableSignal<any | null> = signal(null);
   winningNumber: WritableSignal<number> = signal(-1);
 
   winners: Signal<Player[]> = computed(() =>
@@ -55,4 +53,16 @@ export class StateService {
       ? true
       : false
   );
+
+  resetSession(): void {
+    this.sessionUser.set(null);
+    this.players.set([]);
+    this.userGuess.set(-1);
+    this.session.set(null);
+    this.winningNumber.set(-1);
+  }
+
+  resetUserInfo(): void {
+    this.userInfo.set(null);
+  }
 }
