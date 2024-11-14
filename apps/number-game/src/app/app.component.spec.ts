d24bcdbbdb58a1c473ce1b63ba5a6bc45c1d2fc9
tehw0lf <tehwolf@protonmail.com>
2024-11-15 00:10:44 +0100

    Fix remaining tests

--- apps/number-game/src/app/app.component.spec.ts
+++ apps/number-game/src/app/app.component.spec.ts
@@ -1,12 +1,29 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SessionService } from './services/session.service';
+
+const mockSessionService = {
+  initializeConnection: jest.fn(),
+};
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent, RouterModule.forRoot([])],
+      providers: [{ provide: SessionService, useValue: mockSessionService }],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should initialize a session', () => {
+    expect(mockSessionService.initializeConnection).toHaveBeenCalled();
   });
 });
--- apps/number-game/src/app/home/home.component.spec.ts
+++ apps/number-game/src/app/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Player } from '@number-game/core';
 
+import { SessionService } from '../services/session.service';
 import { HomeComponent } from './home.component';
 
 const mockSessionService = {
@@ -24,6 +25,7 @@ describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomeComponent],
+      providers: [{ provide: SessionService, useValue: mockSessionService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
